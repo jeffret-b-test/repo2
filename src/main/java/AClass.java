0a6aece523c35b7c197f19482142ac5c0b3eb129
Jeff Thompson <jeffret.g@gmail.com>
2019-10-21 16:24:57 -0600

    Convert to Java project.

---
+++ src/main/java/AClass.java
@@ -0,0 +1,12 @@
+public class AClass {
+
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
---
+++ src/test/groovy/AClassTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AClassTest {
+
+    @Test
+    public void getSet() {
+        int a = 572;
+        AClass aClass = new AClass();
+        aClass.setA(a);
+        assertThat(aClass.getA(), is(a));
+    }
+
+}
